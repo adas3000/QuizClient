f921e836972d22be54bb91e35ab5a01b34dc4bf5
Adam_Z <youngtoss38@gmail.com>
2019-12-16 11:58:40 +0100

    dependencies added

--- app/src/main/java/com/quiz/client/MainActivity.kt
+++ app/src/main/java/com/quiz/client/MainActivity.kt
@@ -2,11 +2,16 @@ package com.quiz.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+        
+
     }
 }
