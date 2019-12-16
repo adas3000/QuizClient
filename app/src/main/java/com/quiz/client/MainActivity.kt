e1be46232fd4a32914a1eec22f29d374a302ff7b
Adam_Z <youngtoss38@gmail.com>
2019-12-16 17:23:55 +0100

    adapter added

--- app/src/main/java/com/quiz/client/MainActivity.kt
+++ app/src/main/java/com/quiz/client/MainActivity.kt
@@ -3,16 +3,30 @@ package com.quiz.client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import butterknife.ButterKnife
 import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter:RecyclerView.Adapter<*>
+    private lateinit var viewManager:RecyclerView.LayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         ButterKnife.bind(this)
+
+
+        viewManager = LinearLayoutManager(this)
+
+
+
 
     }
 
---
+++ app/src/main/java/com/quiz/client/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.quiz.client.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.quiz.client.R
+
+class RecyclerViewAdapter(val categoryList:ArrayList<String>):RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.category_layout,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+
+    class ViewHolder(view:View) : RecyclerView.ViewHolder(view){
+            val categoryName = view.findViewById<TextView>(R.id.textView_category)
+
+    }
+
+
+
+}
