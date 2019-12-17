458b595a920ea518c219e798e8ed0fc31ae1aa15
Adam_Z <youngtoss38@gmail.com>
2019-12-17 13:22:18 +0100

    colors update

--- app/src/main/java/com/quiz/client/QuizActivity.kt
+++ app/src/main/java/com/quiz/client/QuizActivity.kt
@@ -2,17 +2,21 @@ package com.quiz.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.quiz.client.adapter.RecyclerViewAnswerAdapter
 import com.quiz.client.adapter.RecyclerViewHeaderAdapter
 import com.quiz.client.model.Question
 import com.quiz.client.util.QuestionListKeeper
+import com.quiz.client.view.IChoiceView
 import kotlinx.android.synthetic.main.activity_quiz.*
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() , IChoiceView {
 
     lateinit var questionList:List<Question>
+    var allQuestionCount:Int=0
+    var correctCount:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,7 @@ class QuizActivity : AppCompatActivity() {
 
 
         questionList = QuestionListKeeper.questionListKeeper
+        allQuestionCount = questionList.size
 
 
         val rv_top =rv_top
@@ -37,4 +42,21 @@ class QuizActivity : AppCompatActivity() {
         rv_chocies.adapter = RecyclerViewAnswerAdapter(questionList.get(0).choices!!.toList())
 
     }
+
+    override fun setNextQuestion(correct: Boolean) {
+
+        allQuestionCount++
+        if(correct){
+            correctCount++
+            Toast.makeText(this,"Good!",Toast.LENGTH_SHORT).show()
+        }
+
+        else {
+            Toast.makeText(this,"Wrong:(",Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+
 }
