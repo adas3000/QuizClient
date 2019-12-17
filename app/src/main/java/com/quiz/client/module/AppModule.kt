0310bd79958e35121584b7e3ef8a708e03bd7a2b
Adam_Z <youngtoss38@gmail.com>
2019-12-17 15:06:30 +0100

    appmodule update

--- app/src/main/java/com/quiz/client/module/AppModule.kt
+++ app/src/main/java/com/quiz/client/module/AppModule.kt
@@ -15,7 +15,7 @@ object AppModule {
         @Singleton
         @Provides
         fun provideRetrofitInstance(): Retrofit {
-            return Retrofit.Builder().baseUrl("http://192.168.0.102:8082")
+            return Retrofit.Builder().baseUrl("http://localhost:8082")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
