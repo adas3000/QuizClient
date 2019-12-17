52b84b83753fb8db0749c06f3c2494667fdba91d
Adam_Z <youngtoss38@gmail.com>
2019-12-17 12:10:24 +0100

    rv header adapter created

--- app/src/main/java/com/quiz/client/QuizActivity.kt
+++ app/src/main/java/com/quiz/client/QuizActivity.kt
@@ -2,8 +2,11 @@ package com.quiz.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.quiz.client.model.Question
 import com.quiz.client.util.QuestionListKeeper
+import kotlinx.android.synthetic.main.activity_quiz.*
 import java.lang.IllegalArgumentException
 
 class QuizActivity : AppCompatActivity() {
@@ -18,7 +21,9 @@ class QuizActivity : AppCompatActivity() {
         questionList = QuestionListKeeper.questionListKeeper
 
 
-
+        val rv =rv_top
+        rv.layoutManager = LinearLayoutManager(this,RecyclerView.HORIZONTAL,false)
+        rv.setHasFixedSize(true)
 
     }
 }
---
+++ app/src/main/java/com/quiz/client/adapter/RecyclerViewHeaderAdapter.kt
@@ -0,0 +1,32 @@
+package com.quiz.client.adapter
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.quiz.client.R
+
+class RecyclerViewHeaderAdapter(val count: Int) : RecyclerView.Adapter<RecyclerViewHeaderAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val v: View = LayoutInflater.from(parent?.context).inflate(R.layout.topchar_layout,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return count
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tv = view.findViewById<TextView>(R.id.textView_square)
+    }
+
+}
