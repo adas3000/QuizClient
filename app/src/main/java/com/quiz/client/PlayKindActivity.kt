19744702ce494a9d3029568a8174b5d5feaaa532
Adam_Z <youngtoss38@gmail.com>
2019-12-29 22:02:49 +0100

    playkindact. update

--- app/src/main/java/com/quiz/client/PlayKindActivity.kt
+++ app/src/main/java/com/quiz/client/PlayKindActivity.kt
@@ -1,11 +1,11 @@
 package com.quiz.client
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.quiz.client.view.IPlayMenuView
+import com.quiz.client.util.hasInternetConnection
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_play_kind.*
 
 class PlayKindActivity : AppCompatActivity(), IPlayMenuView {
@@ -25,10 +25,21 @@ class PlayKindActivity : AppCompatActivity(), IPlayMenuView {
     }
 
     override fun onPlayAlone() {
+
+        if(!hasInternetConnection(this)){
+            Toasty.error(this,"No internet connection",Toasty.LENGTH_SHORT).show()
+            return
+        }
+
         startActivity(Intent(this, PlayActivity::class.java))
     }
 
     override fun onPlayVsOpponent() {
+
+        if(!hasInternetConnection(this)){
+            Toasty.error(this,"No internet connection",Toasty.LENGTH_SHORT).show()
+            return
+        }
 
     }
 
