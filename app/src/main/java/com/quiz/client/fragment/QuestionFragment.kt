61defe5e2f3985da887fca33de51ab629611bf03
Adam_Z <youngtoss38@gmail.com>
2020-01-02 14:08:46 +0100

    activity quiz update

--- app/src/main/java/com/quiz/client/fragment/QuestionFragment.kt
+++ app/src/main/java/com/quiz/client/fragment/QuestionFragment.kt
@@ -11,7 +11,7 @@ class QuestionFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.f_question_layout,container,false)
+        return inflater.inflate(R.layout.activity_quiz,container,false)
     }
 
 
