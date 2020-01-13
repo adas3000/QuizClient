c2f9e407d66f90c00e8e1eb53128335683d80805
Adam_Z <youngtoss38@gmail.com>
2020-01-13 11:06:31 +0100

    new fragment

---
+++ app/src/main/java/com/quiz/client/fragment/WaitFragment.kt
@@ -0,0 +1,17 @@
+package com.quiz.client.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.quiz.client.R
+
+class WaitFragment: Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.layout_wait,container,false)
+    }
+
+}
