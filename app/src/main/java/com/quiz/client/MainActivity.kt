d33ee443e1945d12a33cc33a715fcf3ea4559119
Adam_Z <youngtoss38@gmail.com>
2020-01-15 16:43:08 +0100

    refactor

--- app/src/main/java/com/quiz/client/MainActivity.kt
+++ app/src/main/java/com/quiz/client/MainActivity.kt
@@ -3,6 +3,8 @@ package com.quiz.client
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.room.Room
 import com.google.firebase.FirebaseApp
@@ -37,8 +39,42 @@ class MainActivity : AppCompatActivity() , IMMenuView , IUserView {
 
     override fun onBlankNickName() {
 
+        var nick:String="unknown"
+
+
+
         this.runOnUiThread(Runnable {
-                
+
+            val editText:EditText = EditText(this)
+            val params:LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT)
+
+            val alertDialog:AlertDialog = AlertDialog.Builder(this)
+                .setTitle("Nick")
+                .setMessage("Hello,write your nickname and start QQuuizing!")
+                .setCancelable(false)
+                .setPositiveButton("Confirm",{dialog, which ->
+                    nick = editText.text.toString()
+
+                    if(nick.length < 3){
+                        Toasty.info(applicationContext,"Write at least 3 characters",Toasty.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toasty.success(applicationContext,"Ok",Toasty.LENGTH_SHORT).show()
+                    }
+                })
+                .create()
+
+            editText.layoutParams = params
+            alertDialog.setView(editText)
+
+            alertDialog.show()
+
+
+
+
+
+
         })
 
     }
